d9d2aebdb8d64812fc2dc39fa07a0a658e700f1d
priyankab12 <113348940+priyankab12@users.noreply.github.com>
2023-06-07 11:54:25 +0530

    Add files via upload

---
+++ SeiveOfErotheses.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e7+10;   //generating 10^7 primes
+vector<bool>isPrime(N,1);    //it is telling that initially all elements are primes
+int main()
+{
+  isPrime[0]=isPrime[1]=false;
+  for(int i=2;i<N;i++)
+  {
+    if(isPrime[i]==true)
+    {
+      for(int j=2*i;j<N;j+=i)
+      {
+        isPrime[j]=false;
+      }
+
+    }
+  }
+  for(int i=1;i<30;i++)
+  {
+    cout<<i<<"-> "<<isPrime[i]<<endl;
+  }
+
+
+}
